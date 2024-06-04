a7055c2e2d1d21c4cc111b84baebf1739820eb18
s21sd <srivastavasunny359@gmail.com>
2024-06-04 23:18:43 +0530

    auth completed

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -4,17 +4,21 @@ import prisma from "../../../../../prisma";
 import bcrypt from 'bcrypt'
 export const POST = async (req: Request) => {
     try {
-        const { name, email, password } = await req.json();
+        const { name, email, password, age, weight, gender, heightFeet, heightInches } = await req.json();
         if (!name || !email || !password)
-            return NextResponse.json({
-                message: "Invalid Data",
-            }, {
-                status: 422
-            })
+            return NextResponse.json({ message: "Invalid Data" }, { status: 422 })
         const hashedPassword = await bcrypt.hash(password, 10);
         await connectToDatabase()
-        const newUser = prisma.user.create({ data: { email, name, hashedPassword } })
+        const newUser = await prisma.user.create({ data: { email, name, password: hashedPassword, age, weight, gender, heightFeet, heightInches } })
+        return NextResponse.json({ message: "succesfully registered", newUser }, { status: 201 })
     } catch (error) {
+        console.log(error)
+        return NextResponse.json({
+            message: "Error in the sing up"
 
+        }, { status: 401 })
+    }
+    finally {
+        await prisma.$disconnect();
     }
 }
